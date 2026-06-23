b1c2bcb07ac2209819a9dbf9ae4228b398c64a45
Roman Leventov <leventov.ru@gmail.com>
2026-06-23 17:40:29 +0800

    client: retry write conflict mutations

--- src/server/impl/actions_impl.ts
+++ src/server/impl/actions_impl.ts
@@ -5,6 +5,11 @@ import { parseArgs } from "../../common/index.js";
 import { FunctionReference } from "../../server/api.js";
 import { getFunctionAddress } from "../components/paths.js";
 import { validateArg } from "./validate.js";
+import {
+  retryOnWriteConflict,
+  validateWriteConflictRetryOptions,
+  WriteConflictRetryOptions,
+} from "../../common/write_conflict_retry.js";
 
 function syscallArgs(
   requestId: string,
@@ -35,10 +40,17 @@ export function setupActionCalls(requestId: string) {
     runMutation: async (
       mutation: FunctionReference<"mutation", "public" | "internal">,
       args?: Record<string, Value>,
+      options?: WriteConflictRetryOptions,
     ): Promise<any> => {
-      const result = await performAsyncSyscall(
-        "1.0/actions/mutation",
-        syscallArgs(requestId, mutation, args),
+      const writeConflictRetryOptions =
+        validateWriteConflictRetryOptions(options);
+      const result = await retryOnWriteConflict(
+        () =>
+          performAsyncSyscall(
+            "1.0/actions/mutation",
+            syscallArgs(requestId, mutation, args),
+          ),
+        writeConflictRetryOptions,
       );
       return jsonToConvex(result);
     },
